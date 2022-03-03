d48f5c9377cc1e5574afcddfc5652e4237da35d0
JonAuzmendi <andoitz.goico@gmail.com>
2022-03-03 15:43:58 +0100

    localization estatico funcional

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        //es un método de callback que es llamado cada vez que la vista está renderizada. En este caso recoge el idioma seleccionado y los disponibles
+        //digamos que es parecido a un método async de c#
+        view()->composer('partials.language_switcher', function ($view) {
+            $view->with('current_locale', app()->getLocale());
+            $view->with('available_locales', config('app.available_locales'));
+        });
+    }
+}
